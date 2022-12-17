a018fed0d6e713cb497f2586d3e0bae17b9f440a
shenben <1728816406@qq.com>
2022-12-17 14:28:37 +0000

    CUDA-enabled Torch Backend

--- update.sh
+++ update.sh
@@ -1,4 +1,7 @@
 git add *
 git commit -m 'CUDA-enabled Torch Backend' --no-verify
 git push -u origin master
+
+# git remote -v
+# git remote set-url origin git@github.com:shenben/WasmEdge-shenben.git
 # git remote add origin https://shenben:1HVFr6P2AqD7FEVQZ18fGM3UdnnXV9DeaiyOwMS1BPM@github.com/shenben/WasmEdge-shenben.git
